abed716601f1a6b78c958802f47add5957bcc7a4
viogull <viogull@gmail.com>
2020-03-23 16:58:56 +0200

    UI Skeleton appended.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:fusion_wallet/ui/pages/bottom_home_page.dart';
 import 'package:fusion_wallet/ui/theme/fusion_theme.dart';
+import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 
 import 'localizations.dart';
-
+import 'ui/providers/bottom_navigation_provider.dart';
 
 void main() => runApp(MyApp());
+final logger = Logger();
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: Provider.of<ThemesNotifier>(context).currentThemeData,
-        title: AppLocalizations.of(context).appName(),
-        home: MyHomePage(),
+        theme: FusionTheme.light,
+        home: HomePage(),
         localizationsDelegates: [
           const AppLocalizationsDelegate(),
           GlobalCupertinoLocalizations.delegate,
@@ -27,98 +28,45 @@ class MyApp extends StatelessWidget {
         supportedLocales: [
           const Locale('en', ''),
           const Locale('ru', ''),
-        ]
-    );
+        ]);
   }
-
 }
 
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
 
-class MyHomePage extends StatefulWidget  {
-
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  static const String navId = '/home';
 
   final String title;
 
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-
-
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  static final GlobalKey<ScaffoldState> _scaffoldKey =
+      GlobalKey<ScaffoldState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  var provider = ChangeNotifierProvider<BottomNavigationProvider>(
+    child: BottomHomePage(),
+    create: (context) => BottomNavigationProvider(),
+  );
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
+    logger.d('HomePage[BottomTabsPage, IdentifcationPage] start building.');
+    // This method is rerun every time s etState is called, for instance as done
     // by the _incrementCounter method above.
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: _buildHomepageBody(this.provider),
     );
+  }
+
+  Widget _buildHomepageBody(ChangeNotifierProvider provider) {
+    return provider;
   }
 }
---
+++ lib/ui/pages/auth/passphrase/passphrase_creation_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class PassphraseCreationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+  }
+}
