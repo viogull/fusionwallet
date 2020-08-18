ffe53785cb2489588bf7064f822500aaec0db662
viogull <viogull@gmail.com>
2020-08-18 07:15:24 +0300

    Hotfix v3

--- lib/main.dart
+++ lib/main.dart
@@ -71,34 +71,34 @@ void main() async {
   Hive.registerAdapter<Preferences>(PreferencesAdapter());
   Hive.registerAdapter<AdminNotification>(AdminNotificationAdapter());
 
-  var accsBox = await Hive.openBox<Account>(accountsBox);
+  final accsBox = await Hive.openBox<Account>(accountsBox);
+  try {
+    await Hive.openBox<Contact>(contactsBox);
+    final prefs = await Hive.openBox<Preferences>(preferencesBox);
+    var _prefsSingle =
+        prefs.values.isEmpty ? Preferences() : prefs.values.first;
 
-  await Hive.openBox<Contact>(contactsBox);
-  final prefs = await Hive.openBox<Preferences>(preferencesBox);
-  var prefsSingleton =
-      prefs.values.isEmpty ? Preferences() : prefs.values.first;
+    if (_prefsSingle.isInBox == false) prefs.add(_prefsSingle);
 
-  if (prefsSingleton.isInBox == false)
-    prefs.add(prefsSingleton);
+    await Hive.openBox<AdminNotification>(notificationsBox);
 
+    OneSignal.shared.setLogLevel(OSLogLevel.debug, OSLogLevel.none);
 
-  await Hive.openBox<AdminNotification>(notificationsBox);
+    OneSignal.shared.init("64051c9d-c06c-419d-b1a3-10196218dda3", iOSSettings: {
+      OSiOSSettings.autoPrompt: false,
+      OSiOSSettings.inAppLaunchUrl: true
+    });
+    OneSignal.shared
+        .setInFocusDisplayType(OSNotificationDisplayType.notification);
 
-  OneSignal.shared.setLogLevel(OSLogLevel.debug, OSLogLevel.none);
-
-  OneSignal.shared.init("64051c9d-c06c-419d-b1a3-10196218dda3", iOSSettings: {
-    OSiOSSettings.autoPrompt: false,
-    OSiOSSettings.inAppLaunchUrl: true
-  });
-  OneSignal.shared
-      .setInFocusDisplayType(OSNotificationDisplayType.notification);
-
-  runApp(new StateContainer(
-    child: new App(),
-    accounts: accsBox,
-    preferences: prefsSingleton,
-    alice: injector.get<Alice>()
-  ));
+    runApp(new StateContainer(
+        child: new App(),
+        accounts: accsBox,
+        preferences: _prefsSingle,
+        alice: injector.get<Alice>()));
+  } on Exception catch (exception) {
+    logger.d("Error on start");
+  }
 }
 
 class App extends StatefulWidget {
@@ -107,8 +107,6 @@ class App extends StatefulWidget {
 }
 
 class AppState extends State<App> {
-
-
   @override
   void initState() {
     super.initState();
@@ -170,8 +168,6 @@ class AppState extends State<App> {
 class HomePage extends StatefulWidget {
   static const String navId = '/home';
 
-
-
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -183,7 +179,6 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   );
 
   Timer _timerLink;
-
 
   @override
   void initState() {
@@ -201,21 +196,19 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
     }
   }
 
-
   Future<void> _retrieveDynamicLink() async {
-    final PendingDynamicLinkData data = await FirebaseDynamicLinks.instance.getInitialLink();
+    final PendingDynamicLinkData data =
+        await FirebaseDynamicLinks.instance.getInitialLink();
     final Uri deepLink = data?.link;
     if (deepLink != null) {
       Navigator.pushNamed(context, deepLink.path);
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Material(child: this.provider);
   }
-
 
   @override
   void dispose() {
